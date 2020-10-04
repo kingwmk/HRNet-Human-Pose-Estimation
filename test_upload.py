5273de1ef1d9be463658c2e904eaf8dd2d333e86
kingwmk <1171147450@qq.com>
2020-10-04 21:05:19 +0800

    Update and rename test_upload.txt to test_upload.py

---
+++ test_upload.py
@@ -0,0 +1,8 @@
+import os
+import datatime
+start_time = str(datetime.datetime.now())
+end_time = str(datetime.datetime.now())
+file = open("train_time.txt", "w")
+file.write(start_time)
+file.write(end_time)
+file.close()
