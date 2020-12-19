df7bd36cf0362fa592b5eaabff455288972d049b
kingwmk <1171147450@qq.com>
2020-12-19 23:08:52 +0800

    Update __init__.py

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -12,13 +12,14 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import models.pose_resnet
-import models.pose_hrnet
-import models.new_semhrnet
-import models.new2_semhrnet
-import models.pose_semnet
-import models.semconvhrnet
-import models.semconvhrnet_cat
-import models.base_semhrnet
-import models.base_semhrnet2
-import models.semconvhrnet_mingyuan
+import models.pose_hrnet_aspp
+#import models.pose_resnet
+#import models.pose_hrnet
+#import models.new_semhrnet
+#import models.new2_semhrnet
+#import models.pose_semnet
+#import models.semconvhrnet
+#import models.semconvhrnet_cat
+#import models.base_semhrnet
+#import models.base_semhrnet2
+#import models.semconvhrnet_mingyuan
