e14d12b8792181add2dbd601b60eeca543149732
kingwmk <1171147450@qq.com>
2020-12-20 13:47:48 +0800

    Update __init__.py

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -15,6 +15,7 @@ from __future__ import print_function
 import models.pose_hrnet_aspp
 #import models.pose_resnet
 import models.pose_hrnet
+import models.semconvhrnet_aspp
 #import models.new_semhrnet
 #import models.new2_semhrnet
 #import models.pose_semnet
