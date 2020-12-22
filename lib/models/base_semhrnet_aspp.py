15133de3ca1e9278325eb9eed534317ea3d1eb54
kingwmk <1171147450@qq.com>
2020-12-22 13:25:51 +0800

    Update base_semhrnet_aspp.py

--- lib/models/base_semhrnet_aspp.py
+++ lib/models/base_semhrnet_aspp.py
@@ -26,6 +26,9 @@ class ASPP(nn.Module):
         self.groups = groups
         for dilation, padding in zip(dilation_series, padding_series):
             self.conv2d_list.append(nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=padding, dilation=dilation, groups=groups, bias = True))
+        
+        for m in self.conv2d_list:
+            m.weight.data.normal_(0, 0.01)
 
     def forward(self, x):
         out = self.conv2d_list[0](x)
