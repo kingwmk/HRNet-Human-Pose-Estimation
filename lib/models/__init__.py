2ff38e301e713b2789da19071add164427701412
kingwmk <1171147450@qq.com>
2020-12-29 16:58:07 +0800

    Update __init__.py

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -14,7 +14,7 @@ from __future__ import print_function
 
 import models.base_semhrnet_aspp_pal
 #import models.pose_resnet
-import models.base_semhrnet_aspp_pal+bone
+import models.base_semhrnet_aspp_pal_bone
 import models.semconvhrnet_aspp
 #import models.new_semhrnet
 #import models.new2_semhrnet
