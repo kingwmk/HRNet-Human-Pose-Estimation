d83269bc7dec0fb5ea8aa6f8e043ab1fedf3a933
kingwmk <1171147450@qq.com>
2020-12-29 19:12:10 +0800

    Update base_semhrnet_aspp_pal_bone.py

--- lib/models/base_semhrnet_aspp_pal_bone.py
+++ lib/models/base_semhrnet_aspp_pal_bone.py
@@ -121,8 +121,11 @@ class SemanticMultiGroupConv(nn.Module):
         theta_x = x_vec       
         phi_x = x_vec.permute(0, 2, 1) 
 
-        aff = torch.matmul(theta_x, phi_x).cpu()
-        bone = self.bone.repeat(b,1,1)
+        aff = torch.matmul(theta_x, phi_x)
+        if aff.dtype == 'torch.float32':
+            bone = self.bone.repeat(b,1,1)
+        else:
+            bone = self.bone.cuda().repeat(b,1,1)
         aff = aff + bone
 
         print(aff)
