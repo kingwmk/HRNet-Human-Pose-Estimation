c6d53f9cfca15e67e39fd1e46f05ba3cad2256ec
kingwmk <1171147450@qq.com>
2020-12-31 16:41:02 +0800

    Update __init__.py

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -12,16 +12,16 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import models.base_semhrnet_aspp_spatial_pal
+import models.base_semhrnet_aspp_spatial_pal_pal
 #import models.pose_resnet
-import models.base_semhrnet_aspp_pal_bone
-import models.semconvhrnet_aspp
+#import models.base_semhrnet_aspp_pal_bone
+#import models.semconvhrnet_aspp
 #import models.new_semhrnet
 #import models.new2_semhrnet
 #import models.pose_semnet
-import models.semconvhrnet
+#import models.semconvhrnet
 #import models.semconvhrnet_cat
-import models.base_semhrnet
-import models.t4_base_semhrnet_aspp_block
+#import models.base_semhrnet
+#import models.t4_base_semhrnet_aspp_block
 #import models.base_semhrnet2
 #import models.semconvhrnet_mingyuan
