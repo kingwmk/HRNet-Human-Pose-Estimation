386487bf163fc8b7a97f7d0ba944714d2fcf40ee
kingwmk <1171147450@qq.com>
2021-01-20 09:54:33 +0800

    Update function.py

--- lib/core/function.py
+++ lib/core/function.py
@@ -215,7 +215,7 @@ def multi_scale_semantic_validate(config, val_loader, val_dataset, model, criter
 #                save_debug_images(config, input, meta, target, pred*4, final_heatmaps,
 #                                  prefix)
         print(all_preds.shape)
-        print(all_preds[0])
+        print(all_preds[1])
         name_values, perf_indicator = val_dataset.evaluate(
             config, all_preds, output_dir, all_boxes, image_path,
             filenames, imgnums
